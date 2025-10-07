a3b3855fdb8ac1f30f4b9893deee558a0a069170
echo4eva <perilecho@gmail.com>
2025-10-07 12:40:52 -0700

    actually sending packets via flush

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main()
   enet_address_set_host(&address, "127.0.0.1");
   address.port = 28785;
 
-  peer = enet_host_connect(client, &address, 2, 0);
+  peer = enet_host_connect(client, &address, 3, 0);
   if (peer == nullptr)
   {
     fmt::print("No available peers for initiating an ENet connection.\n");
@@ -45,11 +45,22 @@ int main()
     fmt::print("Connection to host has failed.\n");
     exit (EXIT_FAILURE);
   }
+  enet_host_flush(client);
 
   ENetPacket* packet = enet_packet_create("data", strlen("data"), ENET_PACKET_FLAG_RELIABLE);
-  enet_peer_send(peer, 2, packet);
-  enet_packet_destroy(packet);
   
+  fmt::print("Sending test packet to server\n");
+  int packet_sent = enet_peer_send(peer, 0, packet);
+  if (packet_sent != 0)
+  {
+    fmt::print("Boy what the hell\n");
+  }
+  else
+  {
+    fmt::print("yay\n");
+  }
+  enet_host_flush(client);
+
   enet_host_destroy(client);
   return 0;
 }
