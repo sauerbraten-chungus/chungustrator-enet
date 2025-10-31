4c584fdbed4dbb79e9ac61c9353223eedf56b34d
echo4eva <perilecho@gmail.com>
2025-10-30 18:07:46 -0700

    flight sleep gif

--- src/auth_code_service.cpp
+++ src/auth_code_service.cpp
@@ -1,14 +1,23 @@
 #include "auth_code_service.h"
 #include "enet_client.h"
+#include <chrono>
+#include <fmt/base.h>
+#include <thread>
 
 grpc::Status AuthCodeService::SendAuthCodes(
     grpc::ServerContext* context,
     const chungustrator_enet::AuthCodeRequest* request,
     chungustrator_enet::AuthCodeResponse* response
 ) {
+    const auto& codes = request->codes();
+    for (const auto& pair : codes) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+    std::thread([codes]{
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+            RunENet("127.0.0.1", 28785);
+        }).detach();
+
     response->set_msg("Received");
-
-    RunENet("127.0.0.1", 28785);
-
     return grpc::Status::OK;
 }
