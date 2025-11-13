dcbe42fa8b7a4936f03986d0c485ef01468d30da
echo4eva <perilecho@gmail.com>
2025-11-13 15:39:11 -0800

    clean post-documentation

--- src/main.cpp
+++ src/main.cpp
@@ -22,51 +22,42 @@ void RunServer() {
 
 int main() {
     if (enet_initialize() != 0) {
-        fmt::print("An error occured when initializing ENet.\n");
+        fmt::println("An error occured when initializing ENet.");
     }
     // RunServer();
 
-    ENetAddress server_address;
-    ENetHost *server;
-    server_address.host = ENET_HOST_ANY;
-    // enet_address_set_host(&server_address, "127.0.0.1");
-    server_address.port = 30000;
+    ENetAddress address;
+    ENetHost *host;
+    address.host = ENET_HOST_ANY;
+    address.port = 30000;
 
-    server = enet_host_create(&server_address, 32, 2, 0, 0);
-    if (server == nullptr) {
+    host = enet_host_create(&address, 32, 2, 0, 0);
+    if (host == nullptr) {
         fmt::println("An error occurred while creating the ENet server host.");
         return 1;
     }
-    fmt::println("ENet server listening on {}:{}", server_address.host, server_address.port);
+    fmt::println("ENet server listening on port {}",address.host, address.port);
     fmt::println("Waiting for connections...");
 
     ENetEvent event;
-    while(true) {
-        int result = enet_host_service(server, &event, 1000);
-        if (result < 0) {
-            fmt::println("Error in enet_host_service");
-            break;
-        }
-
-        if (result > 0) {
-            fmt::println("Event received: type={}", (int)event.type);
-            switch (event.type) {
-                case ENET_EVENT_TYPE_CONNECT:
-                    fmt::println("awesome");
-                    break;
-                case ENET_EVENT_TYPE_RECEIVE:
-                    fmt::println("Received packet");
-                    enet_packet_destroy (event.packet);
-                    break;
-                case ENET_EVENT_TYPE_DISCONNECT:
-                    fmt::println("Client disconnected.");
-                    break;
-                case ENET_EVENT_TYPE_NONE:
-                    break;
-            }
+    while(enet_host_service(host, &event, 1000) >= 0) {
+        switch (event.type) {
+            case ENET_EVENT_TYPE_CONNECT:
+                fmt::println("awesome");
+                break;
+            case ENET_EVENT_TYPE_RECEIVE:
+                fmt::println("Received packet");
+                enet_packet_destroy (event.packet);
+                break;
+            case ENET_EVENT_TYPE_DISCONNECT:
+                fmt::println("Client disconnected.");
+                break;
+            case ENET_EVENT_TYPE_NONE:
+                break;
         }
     }
-    enet_host_destroy(server);
+
+    enet_host_destroy(host);
     enet_deinitialize();
 
     return 0;
